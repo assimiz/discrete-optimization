9752b2eb2b2dda1245b666f5d47d6f9cce217b0f
Assaf Mizrachi <assi.mizrachi@gmail.com>
2022-04-11 16:37:10 +0300

    Added debug printing

--- src/mizrachi/discopt/knapsack/AbstractKnapsackSolver.java
+++ src/mizrachi/discopt/knapsack/AbstractKnapsackSolver.java
@@ -17,6 +17,9 @@ public abstract class AbstractKnapsackSolver implements KnapsackSolver {
 	protected int value;
 	protected int[] taken;
 	
+
+	protected boolean debug = false;
+	
 	@Override
 	public void solve (int items, int capacity, int[] values, int[] weights) {
 		this.items = items;
@@ -27,20 +30,26 @@ public abstract class AbstractKnapsackSolver implements KnapsackSolver {
 		this.taken = new int[items];
 		Arrays.fill(this.taken, 0);
 		
+		double startTime = System.currentTimeMillis();
 		run();
 		print();
+		double endTime = System.currentTimeMillis();
+		if (debug) {
+			System.out.println("Computation time[s] = " + (endTime - startTime) / 1000);
+		}
 	}
 
 	
 	abstract protected void run();
+	abstract protected boolean isOptimal();
 	
 	private void print() {
 		// prepare the solution in the specified output format
-        System.out.println(value+" 0");
-        for(int i=0; i < items; i++){
-            System.out.print(taken[i]+" ");
-        }
-        System.out.println("");
+		System.out.println(value + (isOptimal() ? " 1" : " 0"));
+		for (int i = 0; i < items; i++) {
+			System.out.print(taken[i] + " ");
+		}
+		System.out.println("");
 	}
 
 }
